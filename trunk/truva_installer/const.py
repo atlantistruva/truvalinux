dab5582401faf3e5711da11e844059a432774e8d
onur.atlantis <onur.atlantis@f7e353a1-8631-0410-ae36-b9324180a337>
2008-05-15 07:42:34 +0000

    tanimlama dosyasi eklendi...

---
+++ trunk/truva_installer/const.py
@@ -0,0 +1,27 @@
+# path tanimlamalari
+install_dir = "/media/hda6/Depolar/truvalinux-svn/truva_installer/"
+files_dir = install_dir +"files/"
+
+pixmap_dir = install_dir + "pixmaps/"
+
+# kurulum diski ayar dosyasi
+install_disk = files_dir + "kurulum_diski.txt"
+
+# sistem diskleri ayar dosyasi
+disklerim = files_dir + "diskler.txt"
+
+
+# yapilandirma dosyalari dizin tanimlari
+keymap_dir = files_dir + "rc.keymap"
+group_dir = files_dir + "group"
+fstab_dir = files_dir + "fstab"
+xorg_dir = files_dir + "xorg.conf"
+font_dir = files_dir + "rc.font"
+langsh_dir = files_dir + "lang.sh"
+langcsh_dir = files_dir + "lang.csh"
+clock_dir = files_dir + "hardwareclock"
+menulst_dir = files_dir + "menu.lst"
+
+# kurulum dizini baglama
+mntdir = install_dir + "mount"
+
