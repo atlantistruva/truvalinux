e2065037ef977eeb1b1c33c43b039dafad9fb3d9
rohanrhu2 <rohanrhu2@f7e353a1-8631-0410-ae36-b9324180a337>
2008-05-25 17:09:02 +0000

    wlan bilgileri penceresi yazıldı.

--- projeler/network-manager/mainWindow.py
+++ projeler/network-manager/mainWindow.py
@@ -4,23 +4,57 @@
 
 from PyQt4 import QtCore, QtGui
 from ui_mainWindow import Ui_NetworkManager
-from ui_newConnect import Ui_newDialogWindow
+from ui_wlanInfo import Ui_wirelessWindow
 import network
 import pywlan
 
-class newConnectDialog(QtGui.QDialog, Ui_newDialogWindow):
+class wlanInfoWindow(QtGui.QDialog, Ui_wirelessWindow):
 	def __init__(self):
 		QtGui.QDialog.__init__(self)
 		self.setupUi(self)
-		for net in pywlan.listInterfaces():
-			self.nifaceList.addItem(str(net))
+
+		# Signal / Slots
+		QtCore.QObject.connect(self.listWireless, QtCore.SIGNAL("itemClicked(QTableWidgetItem*)"), self.refresh_bar)
+
+		# Show wlans in listWireless
+		self.wireless = pywlan.Wireless("eth1")
+		self.listWireless.setRowCount(len(self.wireless.scanning()))
+		a = 0
+		for i in self.wireless.scanning():
+			HeaderItem = QtGui.QTableWidgetItem()
+			HeaderItem.setText(i)
+			self.listWireless.setVerticalHeaderItem(a, HeaderItem)
+
+			# Wlan infos write to listWireless
+			self.wireless_info = pywlan.wlanInfo("eth1", i)
+			b = 0
+			for it in [self.wireless_info.getEssid(), self.isCrypted("eth1", i), self.wireless_info.getQuality()]:
+				item = QtGui.QTableWidgetItem()
+				item.setText(str(it))
+				self.listWireless.setItem(a, b, item)
+				b += 1
+			a += 1
+
+	def refresh_bar(self):
+		print "..."
+		self.qualityBar.setValue(int(pywlan.wlanInfo("eth1", str(self.listWireless.selectedItems()[0].text())).getQuality()))
+
+	def isCrypted(self, iface, wlan):
+		if pywlan.wlanInfo(iface, wlan).isEncryption():
+			return "Evet"
+		else:
+			return "Hayır"
+
+			a += 1
 
 class mainWindow(QtGui.QMainWindow, Ui_NetworkManager):
 	def __init__(self):
 		QtGui.QMainWindow.__init__(self)
 		self.setupUi(self)
 		QtCore.QObject.connect(self.newButton, QtCore.SIGNAL("clicked(bool)"), self.newConnect)
-		QtCore.QObject.connect(self.connectButton, QtCore.SIGNAL("clicked(bool)"), self.deneme)
+		QtCore.QObject.connect(self.connectButton, QtCore.SIGNAL("clicked(bool)"), self.showInfo)
+
+		self.wlanInfoWindow = wlanInfoWindow()
 
 		self.allifaces = network.Networking_Info().listIfaces()
 
@@ -35,7 +69,7 @@ class mainWindow(QtGui.QMainWindow, Ui_NetworkManager):
 			headerItem.setText(i)
 			self.listNetworks.setVerticalHeaderItem(a, headerItem)
 
-			# Bağlantı bilgileri yazdırılıyor...
+			# Write connecting info to listNetwroks
 			b = 0
 			for it in self.allifaces[i]:
 				item = QtGui.QTableWidgetItem()
@@ -45,8 +79,9 @@ class mainWindow(QtGui.QMainWindow, Ui_NetworkManager):
 
 			a += 1
 
-	def deneme(self):
-		print "..."
+	def showInfo(self):
+		self.selecteditem = str(self.listNetworks.selectedItems()[0].text())
+		self.wlanInfoWindow.show()
 
 	def newConnect(self):
 		self.newConnectWindow = newConnectDialog()
---
+++ projeler/network-manager/ui_wlanInfo.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'wlanInfo.ui'
+#
+# Created: Sun May 25 19:46:26 2008
+#      by: PyQt4 UI code generator 4.3.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+class Ui_wirelessWindow(object):
+    def setupUi(self, wirelessWindow):
+        wirelessWindow.setObjectName("wirelessWindow")
+        wirelessWindow.resize(QtCore.QSize(QtCore.QRect(0,0,431,405).size()).expandedTo(wirelessWindow.minimumSizeHint()))
+
+        self.connectInfoBox = QtGui.QGroupBox(wirelessWindow)
+        self.connectInfoBox.setGeometry(QtCore.QRect(10,10,401,121))
+        self.connectInfoBox.setObjectName("connectInfoBox")
+
+        self.label = QtGui.QLabel(self.connectInfoBox)
+        self.label.setGeometry(QtCore.QRect(10,20,101,18))
+        self.label.setObjectName("label")
+
+        self.networkIfaceLabel = QtGui.QLabel(self.connectInfoBox)
+        self.networkIfaceLabel.setGeometry(QtCore.QRect(120,20,91,18))
+        self.networkIfaceLabel.setObjectName("networkIfaceLabel")
+
+        self.label_2 = QtGui.QLabel(self.connectInfoBox)
+        self.label_2.setGeometry(QtCore.QRect(10,50,101,18))
+        self.label_2.setObjectName("label_2")
+
+        self.connectedNetLabel = QtGui.QLabel(self.connectInfoBox)
+        self.connectedNetLabel.setGeometry(QtCore.QRect(120,50,54,18))
+        self.connectedNetLabel.setObjectName("connectedNetLabel")
+
+        self.label_3 = QtGui.QLabel(self.connectInfoBox)
+        self.label_3.setGeometry(QtCore.QRect(200,20,71,18))
+        self.label_3.setObjectName("label_3")
+
+        self.isCryptLabel = QtGui.QLabel(self.connectInfoBox)
+        self.isCryptLabel.setGeometry(QtCore.QRect(300,20,54,18))
+        self.isCryptLabel.setObjectName("isCryptLabel")
+
+        self.label_4 = QtGui.QLabel(self.connectInfoBox)
+        self.label_4.setGeometry(QtCore.QRect(200,50,81,18))
+        self.label_4.setObjectName("label_4")
+
+        self.label_5 = QtGui.QLabel(self.connectInfoBox)
+        self.label_5.setGeometry(QtCore.QRect(10,80,91,18))
+        self.label_5.setObjectName("label_5")
+
+        self.bssidLabel = QtGui.QLabel(self.connectInfoBox)
+        self.bssidLabel.setGeometry(QtCore.QRect(120,80,54,18))
+        self.bssidLabel.setObjectName("bssidLabel")
+
+        self.qualityLabel = QtGui.QLabel(self.connectInfoBox)
+        self.qualityLabel.setGeometry(QtCore.QRect(300,50,54,18))
+        self.qualityLabel.setObjectName("qualityLabel")
+
+        self.listWireless = QtGui.QTableWidget(wirelessWindow)
+        self.listWireless.setGeometry(QtCore.QRect(10,150,401,171))
+        self.listWireless.setObjectName("listWireless")
+
+        self.label_6 = QtGui.QLabel(wirelessWindow)
+        self.label_6.setGeometry(QtCore.QRect(10,130,101,18))
+        self.label_6.setObjectName("label_6")
+
+        self.label_7 = QtGui.QLabel(wirelessWindow)
+        self.label_7.setGeometry(QtCore.QRect(10,350,91,18))
+        self.label_7.setObjectName("label_7")
+
+        self.qualityBar = QtGui.QProgressBar(wirelessWindow)
+        self.qualityBar.setGeometry(QtCore.QRect(10,370,401,23))
+        self.qualityBar.setProperty("value",QtCore.QVariant(0))
+        self.qualityBar.setObjectName("qualityBar")
+
+        self.refreshWirelessButton = QtGui.QPushButton(wirelessWindow)
+        self.refreshWirelessButton.setGeometry(QtCore.QRect(330,330,75,28))
+        self.refreshWirelessButton.setObjectName("refreshWirelessButton")
+
+        self.line = QtGui.QFrame(wirelessWindow)
+        self.line.setGeometry(QtCore.QRect(310,330,20,31))
+        self.line.setFrameShape(QtGui.QFrame.VLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName("line")
+
+        self.connectWirelessButton = QtGui.QPushButton(wirelessWindow)
+        self.connectWirelessButton.setGeometry(QtCore.QRect(230,330,75,28))
+        self.connectWirelessButton.setObjectName("connectWirelessButton")
+
+        self.retranslateUi(wirelessWindow)
+        QtCore.QMetaObject.connectSlotsByName(wirelessWindow)
+
+    def retranslateUi(self, wirelessWindow):
+        wirelessWindow.setWindowTitle(QtGui.QApplication.translate("wirelessWindow", "Wireless Ağı", None, QtGui.QApplication.UnicodeUTF8))
+        self.connectInfoBox.setTitle(QtGui.QApplication.translate("wirelessWindow", "Bağlantı durumu:", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("wirelessWindow", "Bağlantı arayüzü:", None, QtGui.QApplication.UnicodeUTF8))
+        self.networkIfaceLabel.setText(QtGui.QApplication.translate("wirelessWindow", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("wirelessWindow", "Bağlı olunan ağ:", None, QtGui.QApplication.UnicodeUTF8))
+        self.connectedNetLabel.setText(QtGui.QApplication.translate("wirelessWindow", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("wirelessWindow", "Şifrelenmiş:", None, QtGui.QApplication.UnicodeUTF8))
+        self.isCryptLabel.setText(QtGui.QApplication.translate("wirelessWindow", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_4.setText(QtGui.QApplication.translate("wirelessWindow", "Sinyal kalitesi:", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_5.setText(QtGui.QApplication.translate("wirelessWindow", "Makine adresi:", None, QtGui.QApplication.UnicodeUTF8))
+        self.bssidLabel.setText(QtGui.QApplication.translate("wirelessWindow", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.qualityLabel.setText(QtGui.QApplication.translate("wirelessWindow", "...", None, QtGui.QApplication.UnicodeUTF8))
+        self.listWireless.clear()
+        self.listWireless.setColumnCount(3)
+        self.listWireless.setRowCount(0)
+
+        headerItem = QtGui.QTableWidgetItem()
+        headerItem.setText(QtGui.QApplication.translate("wirelessWindow", "Essid", None, QtGui.QApplication.UnicodeUTF8))
+        self.listWireless.setHorizontalHeaderItem(0,headerItem)
+
+        headerItem1 = QtGui.QTableWidgetItem()
+        headerItem1.setText(QtGui.QApplication.translate("wirelessWindow", "Şifreli ?", None, QtGui.QApplication.UnicodeUTF8))
+        self.listWireless.setHorizontalHeaderItem(1,headerItem1)
+
+        headerItem2 = QtGui.QTableWidgetItem()
+        headerItem2.setText(QtGui.QApplication.translate("wirelessWindow", "Sİnyal kalitesi", None, QtGui.QApplication.UnicodeUTF8))
+        self.listWireless.setHorizontalHeaderItem(2,headerItem2)
+        self.label_6.setText(QtGui.QApplication.translate("wirelessWindow", "Tarama sonuçları:", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_7.setText(QtGui.QApplication.translate("wirelessWindow", "Sinyal kalitesi:", None, QtGui.QApplication.UnicodeUTF8))
+        self.refreshWirelessButton.setText(QtGui.QApplication.translate("wirelessWindow", "Yenile", None, QtGui.QApplication.UnicodeUTF8))
+        self.connectWirelessButton.setText(QtGui.QApplication.translate("wirelessWindow", "Bağlan", None, QtGui.QApplication.UnicodeUTF8))
+
