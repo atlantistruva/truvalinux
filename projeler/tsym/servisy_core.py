bf005149e7883e9d534ea2361918e2e204d7d7ce
rohanrhu2 <rohanrhu2@f7e353a1-8631-0410-ae36-b9324180a337>
2008-06-17 13:15:42 +0000

    genel iyileştirmeler.

--- projeler/tsym/servisy_core.py
+++ projeler/tsym/servisy_core.py
@@ -1,23 +1,35 @@
 # -*- coding: utf-8 -*-
 # Truva Linux servis yöneticisi uygulaması
 # Temel bileşenler modülü
-# Author: Oğuzhan Eroğlu (oguzhan@oguzhaneroglu.com)
+#  Copyright (C) 2008  Truva Linux
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
 import shelve
 
 db = shelve.open("%s/.servisy.db" % os.environ["HOME"])
-servis_dizin = "/etc/init.d/"
+servis_dizin = "/etc/rc.d/"
 
 class Yonetici:
 	def __init__(self):
 		self.db = db
 		self.servis_dizin = servis_dizin
 
-	def calistir(self, servis_adi, s_komut):
-		if os.path.exists("%s%s" % (self.servis_dizin, servis_adi)):
-			os.system("sh %s%s %s > /dev/null" % (self.servis_dizin, servis_adi, s_komut))
+	def calistir(self, servisadi, s_komut):
+		print "%s%s" % (self.servis_dizin, "rc." + servisadi)
+		if os.path.exists("%s%s" % (self.servis_dizin, "rc." + servisadi)):
+			print "..."
+			os.system("sh %s%s %s > /dev/null" % (self.servis_dizin, "rc." + servisadi, s_komut))
 		else:
 			print "Servis bulunamadı!"
 
@@ -25,34 +37,34 @@ class Yonetici:
 		for i in self.db["acilis"]:
 			print i
 
-	def acilis_ekle(self, servis_adi):
-		self.db["acilis"].append(servis_adi)
+	def acilis_ekle(self, servisadi):
+		self.db["acilis"].append("rc." + servisadi)
 
-	def servis_baslat(self, servis_adi):
-		self.calistir(servis_adi, "start")
-		print servis_adi + " servisi başlatıldı."
+	def servis_baslat(self, servisadi):
+		self.calistir(servisadi, "start")
+		print "rc." + servisadi + " servisi başlatıldı."
 
-	def servis_aktif(self, servis_adi):
-		os.chmod(self.servis_dizin + servis_adi, 0777)
-		print servis_adi + " servisi açılışta başlatılacak."
+	def servis_aktif(self, servisadi):
+		os.chmod(self.servis_dizin + "rc." + servisadi, 0777)
+		print "rc." + servisadi + " servisi açılışta başlatılacak."
 
-	def servis_deaktif(self, servis_adi):
-		os.chmod(self.servis_dizin + servis_adi, 777)
-		print servis_adi + " servisi açılışta başlatılmayacak."
+	def servis_deaktif(self, servisadi):
+		os.chmod(self.servis_dizin + "rc." + servisadi, 777)
+		print "rc." + servisadi + " servisi açılışta başlatılmayacak."
 
-	def servis_durdur(self, servis_adi):
-		self.calistir(servis_adi, "stop")
-		print servis_adi + " servisi durduruldu."
+	def servis_durdur(self, servisadi):
+		self.calistir(servisadi, "stop")
+		print "rc." + servisadi + " servisi durduruldu."
 
 	def listele(self):
 		return os.listdir(self.servis_dizin)
 
-	def durum(self, servis_adi):
-		return os.popen("ps -e|grep -c %s" % servis_adi).read().replace("\n", "")
+	def durum(self, servisadi):
+		return os.popen("ps -e|grep -c %s" % servisadi).read().replace("\n", "")
 
-	def restart(self, servis_adi):
-		self.servis_durdur(servis_adi)
-		self.servis_baslat(servis_adi)
+	def restart(self, servisadi):
+		self.servis_durdur("rc." + servisadi)
+		self.servis_baslat("rc." + servisadi)
 
 	def listele_goster(self):
 		self.listeleg = self.listele()
--- projeler/tsym/servisy_gui.py
+++ projeler/tsym/servisy_gui.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
-# Truva Linux servis yöneticisi grafik kullanıcı arabirimi
-# Author: Oğuzhan Eroğlu (oguzhan@oguzhaneroglu.com)
+# Truva Linux servis yöneticisi uygulaması
+# Grafik arabirimi
+#  Copyright (C) 2008  Truva Linux
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from PyQt4 import QtCore, QtGui
 from ui_servisy import Ui_ServisYoneticisi
@@ -39,7 +50,7 @@ except(IndexError):
 		# TODO: Servis durumu gösterilecek.
 		self.servisIsim = str(self.servisList.selectedItems()[0].text())
 		self.sad_Label.setText(self.servisIsim)
-		if self.durum(self.servisIsim) == "0":
+		if self.durum(self.servisIsim[2:]) == "0":
 			self.durumLabel.setText(u"Çalışmıyor")
 		else:
 			self.durumLabel.setText(u"Çalışıyor")
